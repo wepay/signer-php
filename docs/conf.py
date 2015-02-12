9a47a1064e7e17d03ab116c3400161d591f1b2cb
Ryan Parman <ryan.parman@wepay.com>
2015-02-12 00:48:42 -0800

    Second pass at some initial docs.

---
+++ docs/conf.py
@@ -0,0 +1,26 @@
+import sys, os
+from sphinx.highlighting import lexers
+from pygments.lexers.web import PhpLexer
+
+lexers['php'] = PhpLexer(startinline=True, linenos=1)
+lexers['php-annotations'] = PhpLexer(startinline=True, linenos=1)
+primary_domain = 'php'
+
+extensions = []
+templates_path = ['_templates']
+source_suffix = '.rst'
+master_doc = 'index'
+project = u'WePay Signer for PHP'
+copyright = u'2015 WePay'
+version = '1.0.0'
+html_title = "WePay Signer for PHP Documentation"
+html_short_title = "WePay Signer for PHP"
+
+exclude_patterns = ['_build']
+
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+
+if not on_rtd:  # only import and set the theme if we're building docs locally
+    import sphinx_rtd_theme
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
