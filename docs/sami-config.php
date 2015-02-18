43f3c94634da9ab0924ce10abd86f9b88c327df3
Ryan Parman <ryan.parman@wepay.com>
2015-02-17 16:16:10 -0800

    Adding support for Sami.

---
+++ docs/sami-config.php
@@ -0,0 +1,29 @@
+<?php
+
+use Sami\Sami;
+use Sami\Version\GitVersionCollection;
+use Symfony\Component\Finder\Finder;
+
+$iterator = Finder::create()
+    ->files()
+    ->name('*.php')
+    ->exclude('tests')
+    ->in(dirname(__DIR__) . '/src')
+    // ->in(dirname(__DIR__) . '/vendor')
+;
+
+$repo = dirname(__DIR__);
+
+$versions = GitVersionCollection::create($repo)
+    ->addFromTags('1.0.*')
+    ->add('master', 'master branch')
+;
+
+return new Sami($iterator, array(
+    'theme'                => 'symfony',
+    'versions'             => $versions,
+    'title'                => 'WePay Signer for PHP',
+    'build_dir'            => __DIR__.'/api-build/%version%',
+    'cache_dir'            => __DIR__.'/api-cache/%version%',
+    'default_opened_level' => 2,
+));
