e1540508ebbf6db92d4970b342c78ed2d8b4bc0a
John Jin <johnj@gofundme.com>
2015-10-28 18:05:00 -0700

    use http_build_query to build query string

--- src/Signer/Signer.php
+++ src/Signer/Signer.php
@@ -123,11 +123,6 @@ class Signer implements SignerInterface, LoggerAwareInterface
 
         ksort($payload);
 
-        $qsa = [];
-        foreach ($payload as $k => $v) {
-            $qsa[] = sprintf('%s=%s', $k, $v);
-        }
-
-        return implode('&', $qsa);
+        return http_build_query($payload);
     }
 }
